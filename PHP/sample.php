6b6a6d65bf62339155badc3d85f56828a13a1333
DecentRandom <misskiwi@decentrandom.com>
2019-06-05 16:50:43 +0900

    Update sample.php

--- PHP/sample.php
+++ PHP/sample.php
@@ -1,5 +1,18 @@
 <?
 
-
+// Parser Library
+include "./lib_decentrand.php";
 
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>DecentRandom Parser Sample</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+</body>
+
+</html>
