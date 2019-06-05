e2cb3734c87b48aabb81eab583122d5c93c6ff6e
DecentRandom <misskiwi@decentrandom.com>
2019-06-05 17:02:38 +0900

    Update sample.php

--- PHP/sample.php
+++ PHP/sample.php
@@ -3,8 +3,11 @@
 // Parser Library
 include "./lib_decentrand.php";
 
-?>
-<!DOCTYPE html>
+$_rand_hash = $_GET["rand_hash"];
+$_range_min = $_GET["range_min"];
+$_range_max = $_GET["range_max"];
+
+?><!DOCTYPE html>
 <html>
 
 <head>
@@ -13,6 +16,21 @@ include "./lib_decentrand.php";
 </head>
 
 <body>
+<form method="get" action="./sample.php">
+    <div>검증인이 제시한 해시 : <input type="text" name="rand_hash" size="64"></div>
+    <div>난수 범위(최소) : <input type="text" name="range_min" size="5"></div>
+    <div>난수 범위(최대) : <input type="text" name="range_max" size="5"></div>
+    <div><button type="submit">난수 구하기</button></div>
+</form>
+<?
+
+if (trim($_rand_hash) != "" && $_range_min >= 0 && $_range_max > $_range_min) {
+
+    $result = get_simple_rand($_rand_hash, $_range_min, $_range_max);
+    echo "<div>선택된 난수 : <b>".$result."</b></div>";
+}
+
+?>
 </body>
 
 </html>
