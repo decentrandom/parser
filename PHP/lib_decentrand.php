7c76d31778ed1a4cc815d15a09c03e49b9a5f829
decentrandom <misskiwi@decentrandom.com>
2019-09-01 16:02:32 +0900

    Update lib_decentrand.php

--- PHP/lib_decentrand.php
+++ PHP/lib_decentrand.php
@@ -1,11 +1,17 @@
 <?
 
-function get_simple_rand($hash, $min, $max) {
+function get_simple_rand($nonce, $min, $max, $seed) {
 
-    $hash = trim($hash);
-    $hash = preg_replace("/\W|_/", "", $hash);
-    $hash = strtoupper($hash);
+    $nonce = trim($nonce);
+    $nonce = preg_replace("/\W|_/", "", $nonce);
+    $nonce = strtoupper($nonce);
     
+    $seed = trim($seed);
+    $seed = preg_replace("/\W|_/", "", $seed);
+    $seed = strtoupper($seed);
+    
+    $hash = $nonce.$seed;
+
     $length = strlen($hash);
 
     if ($length == 0) {
@@ -35,6 +41,51 @@ function get_simple_rand($hash, $min, $max) {
     }
 
     return ($sum % $range) + $min;
+}
+
+
+function make_simple_pick($nonce, $target, $seed) {
+	
+	$nonce = trim($nonce);
+    $nonce = preg_replace("/\W|_/", "", $nonce);
+    $nonce = strtoupper($nonce);
+    
+	$seed = trim(md5($seed));
+    $seed = preg_replace("/\W|_/", "", $seed);
+    $seed = strtoupper($seed);
+    
+    $target = trim($target);
+    $array_target = explode(",", $target);
+    
+    $number_of_tartets = count($array_target);
+    
+    $array_hash = array();
+    $array_alphabet = range ('A', 'Z');
+    
+    for ($i = 0; $i < $number_of_tartets; $i++) {
+	    
+	    $hash = strtoupper(md5($nonce.$array_target[$i].$seed));
+	    $length = strlen($hash);
+	    $sum = 0;
+	    
+	    for ($j = 0; $j < $length; $j++) {
+		    
+		    $character = substr($hash, $j, 1);
+		    
+		    if (ctype_alpha($character)) {
+           		$sum += array_search($character, $array_alphabet) + 10;
+			} else {
+           		$sum += (int)$character;
+		   	}
+	    }
+	    
+	    $array_hash[$i] = $sum;
+    }
+    
+    array_multisort($array_hash, SORT_DESC, $array_target);
+    
+    return $array_target;
+    
 }
 
 ?>
